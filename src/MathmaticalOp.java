3bb37cb36243e51dd5485ff7d3ba0a617d0955d2
Viraj <virajsrv@gmail>
2022-06-08 16:09:48 +0530

    Mathematical Operations

---
+++ src/MathmaticalOp.java
@@ -0,0 +1,14 @@
+public class MathmaticalOp {
+    public static void main(String[] args) {
+        int a = 52, b = 85, result;
+        //Addition
+        result = a + b;
+        System.out.println("Addition is " + result);
+        result = a - b;
+        System.out.println("Substraction is " + result);
+        result = a * b;
+        System.out.println("Multiplication is " + result);
+        result = a / b;
+        System.out.println("Division is " + result);
+    }
+}
